bdbb8b8fa1228f70db80d4a1f70a8d197ae90a23
cupdoya <cupdoya@naver.com>
2021-11-15 10:50:41 +0900

    자동 로그인 수정

--- ModalForm.cs
+++ ModalForm.cs
@@ -21,82 +21,82 @@ namespace Modal.test
             LoadLoginData();
         }
 
-        private void LoadLoginData()
-        {
+        private void LoadLoginData() {
             StreamWriter sw = new StreamWriter(new FileStream("login.dat", FileMode.Append)); // 처음 프로그램 실행 시 login.dat 생성 위함
             sw.Close();
 
             StreamReader sr = new StreamReader(new FileStream("login.dat", FileMode.Open));
 
-            string a = sr.ReadLine(); // 파일 구조 [ 체크박스 체크여부(1 or 0) / ID / PW ]
-            if (a == "1")
-            {
+            string a = sr.ReadLine(); // 파일 구조 [ 체크박스 체크여부(1 or 0) / ID / PW(쿼리문을 통한 암호화, 복호화 처리) ]
+            if (a == "1") {
                 textBoxID.Text = sr.ReadLine();
-                textBoxPW.Text = sr.ReadLine();
                 sr.Close();
+
+                using (MySqlConnection conn = new MySqlConnection("Server = 27.96.130.41; Port = 3306; Database = s5584534; Uid = s5584534; Pwd = s5584534; Charset = utf8;")) {
+                    conn.Open();
+                    string query = "SELECT * FROM s5584534.user WHERE userID='" + textBoxID.Text + "'";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                        textBoxPW.Text = rdr["userPW"] + "";
+
+                    rdr.Close();
+                }
 
                 checkBoxAutoLogin.Checked = true;
             }
 
-            else
-            {
+            else {
                 sr.Close();
                 sw = new StreamWriter(new FileStream("login.dat", FileMode.Create));
 
                 sw.WriteLine("0");
                 sw.WriteLine("");
-                sw.WriteLine("");
 
                 sw.Close();
             }
         }
-        private void buttonLogin_Click(object sender, EventArgs e)
-        {
+        private void buttonLogin_Click(object sender, EventArgs e) {
             string strconn = "Server = 27.96.130.41;Port = 3306;Database = s5584534;Uid = s5584534;Pwd=s5584534;Charset=utf8;";
 
-            using (MySqlConnection connection = new MySqlConnection(strconn))
-            {
-                string insertQuery = "Select Count(*) from user where userID = '" + textBoxID.Text + "' and userPW = '" + textBoxPW.Text + "'";
+            using (MySqlConnection connection = new MySqlConnection(strconn)) {
+                string insertQuery = "Select Count(*) from user where UserID = '" + textBoxID.Text + "' and UserPW = '" + textBoxPW.Text + "'";
 
                 connection.Open();
                 MySqlCommand command = new MySqlCommand(insertQuery, connection);
 
                 MySqlDataReader rdr = command.ExecuteReader();
                 rdr.Read();
-                if (rdr[0].ToString() == "1")
-                {
+                if (rdr[0].ToString() == "1") {
                     //로그인 성공
                     this.Hide();
 
                     this.Close();
+
+                    if (checkBoxAutoLogin.Checked == true) {
+                        StreamWriter sw = new StreamWriter(new FileStream("login.dat", FileMode.Create));
+
+                        sw.WriteLine("1");
+                        sw.WriteLine(textBoxID.Text);
+
+                        sw.Close();
+                    }
+
+                    else {
+                        StreamWriter sw = new StreamWriter(new FileStream("login.dat", FileMode.Create));
+
+                        sw.WriteLine("0");
+                        sw.WriteLine("");
+
+                        sw.Close();
+                    }
                 }
-                else
-                {
+                else {
                     //로그인 실패
                     MessageBox.Show("아이디와 비밀번호를 확인해주세요.");
                 }
                 connection.Close();
-            }
-            if (checkBoxAutoLogin.Checked == true)
-            {
-                StreamWriter sw = new StreamWriter(new FileStream("login.dat", FileMode.Create));
-
-                sw.WriteLine("1");
-                sw.WriteLine(textBoxID.Text);
-                sw.WriteLine(textBoxPW.Text);
-
-                sw.Close();
-            }
-
-            else
-            {
-                StreamWriter sw = new StreamWriter(new FileStream("login.dat", FileMode.Create));
-
-                sw.WriteLine("0");
-                sw.WriteLine("");
-                sw.WriteLine("");
-
-                sw.Close();
             }
         }
 
