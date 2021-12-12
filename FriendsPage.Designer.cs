f1fa6594269dd8852f05c7404d44d4bbd59a3360
Bang57 <90671160+Bang57@users.noreply.github.com>
2021-12-12 19:19:10 +0900

    friendPage.Designer label1 원상복구

--- FriendsPage.Designer.cs
+++ FriendsPage.Designer.cs
@@ -35,11 +35,10 @@ namespace DBP
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(15, 13);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어 ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 29);
+            this.label1.Size = new System.Drawing.Size(87, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "친구 목록";
             // 
