d39b81e4a932a8d8c2019c4bfc4561f8dc7ec150
[JoSeongMin] <dc05114@gmail.com>
2021-12-13 03:34:05 +0900

    ¿complete

--- ChatForm.cs
+++ ChatForm.cs
@@ -427,46 +427,18 @@ namespace DBP
         // 다운로드 버튼
         private void Filebutton_Click(object sender, EventArgs e)
         {
-            /*Uri sourceFileUri = new Uri(118.67.142.129:22); FtpWebRequest ftpWebRequest = WebRequest.Create(sourceFileUri) as FtpWebRequest;
-            ftpWebRequest.Credentials = new NetworkCredential(userID, password);
-            ftpWebRequest.Method = WebRequestMethods.Ftp.DownloadFile;
-            FtpWebResponse ftpWebResponse = ftpWebRequest.GetResponse() as FtpWebResponse;
-            Stream sourceStream = ftpWebResponse.GetResponseStream();
-            FileStream targetFileStream = new FileStream(targetFilePath, FileMode.Create, FileAccess.Write);
-            byte[] bufferByteArray = new byte[1024];
-            while (true) 
-            { 
-                int byteCount = sourceStream.Read(bufferByteArray, 0, bufferByteArray.Length);
-                if (byteCount == 0) { break; }
-                targetFileStream.Write(bufferByteArray, 0, byteCount);
-            }
-            targetFileStream.Close(); 
-            sourceStream.Close();*/
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket.Connect(new IPEndPoint(IPAddress.Parse("118.67.142.129"), 1028));
+            string localpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
 
             int contentByteLength = 0;
-            FileStream fs = new FileStream(@"C:\Users\tiger\temp.zip", FileMode.Create, FileAccess.Write);
+            FileStream fs = new FileStream(localpath+"/DBP_.zip", FileMode.Create, FileAccess.Write);
 
-            /*byte[] receive = new byte[10240000];
-            socket.Receive(receive);
-            int index = 0;
-            for (int i = 0; i < 10240000; i++)
-            {
-                if (receive[i] == '0')
-                {
-                    break;
-                }
-                index = i;
-            }
-            BinaryWriter bw = new BinaryWriter(fs);
-            bw.Write(receive, 0, 576325);*/
-
-            int recv = 0;
             int brk = 0;
             while (contentByteLength < 10240000)
             {
-                Thread.Sleep(75);
+                Thread.Sleep(85);
                 if (socket.Available > 0)
                 {
                     byte[] receive = new byte[socket.Available];
@@ -481,6 +453,7 @@ namespace DBP
 
             Console.WriteLine("다운로드 끝");
             fs.Close();
+            socket.Close();
         }
 
         int flag = 0;
@@ -526,6 +499,7 @@ namespace DBP
                 string filepath = openFileDialog.FileName;
                 string fileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
                 MainForm.mainform.SendMessage(receiverName, "[ZIP]"+filepath+"]"+fileName, "0");  // zip부터 보내고 알아듣게 하기
+                MainForm.mainform.SendMessage(filepath, fileName, "1");                           // zip 보내기
             }
         }
 
